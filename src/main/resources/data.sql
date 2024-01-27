b248b9f0eb95342c71c1275b10789da53d4c2eb7
Pedro Azin <pedroazin@gmail.com>
2024-01-27 12:10:11 -0300

    First version

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-insert into pessoa (id,nome) values (1,'Pedro Azin'), (2,'Taina Azin'), (3,'Felipe Azin'), (4,'Jacqueline Azin') , (5,'Maria Isabel Aperecida dos Santos');
+insert into pessoa (id,nome) values (1,'Pedro Azin'), (2,'Taina Azin'), (3,'Felipe Azin'), (4,'Jacqueline Azin') , (5,'Maria Isabel Aperecida dos Santos')
+,(6,'Carlos Jose');
